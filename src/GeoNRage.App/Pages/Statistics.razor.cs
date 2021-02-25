6f8c58562af3e54f4d0b975f085572aabbc4b1fd
Kévin Gallienne <k94ll13nn3@outlook.fr>
2021-02-25 19:03:51 +0100

    Update statistics page

--- src/GeoNRage.App/Pages/Statistics.razor.cs
+++ src/GeoNRage.App/Pages/Statistics.razor.cs
@@ -14,20 +14,22 @@ namespace GeoNRage.App.Pages
 
         public IEnumerable<GameDto> Games { get; set; } = Enumerable.Empty<GameDto>();
 
-        public Dictionary<(int id, string name), IList<int>> PlayersWithScores { get; } = new();
+        public IEnumerable<PlayerDto> Players { get; set; } = Enumerable.Empty<PlayerDto>();
+
+        public IList<int> GetScores(int playerId)
+        {
+            return Games
+                .Select(g => g.Values.Where(v => v.PlayerId == playerId && g.Maps.Select(m => m.Id).Contains(v.MapId)))
+                .SelectMany(v => v)
+                .Select(v => v.Score)
+                .ToList();
+        }
 
         protected override async Task OnInitializedAsync()
         {
             Games = await GamesApi.GetAllAsync();
 
-            foreach ((int id, string name) in Games.SelectMany(g => g.Players.Select(p => (p.Id, p.Name))))
-            {
-                PlayersWithScores[(id, name)] = Games
-                    .Select(g => g.Values.Where(v => v.PlayerId == id && g.Maps.Select(m => m.Id).Contains(v.MapId)))
-                    .SelectMany(v => v)
-                    .Select(v => v.Score)
-                    .ToList();
-            }
+            Players = Games.SelectMany(g => g.Players).Distinct();
         }
     }
 }
