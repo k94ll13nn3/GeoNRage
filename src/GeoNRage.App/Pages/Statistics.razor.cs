d27d0127b06b75342d3c3747649268f316b657a4
Kévin Gallienne <k94ll13nn3@outlook.fr>
2021-05-29 17:53:41 +0200

    Add sort to statistics page

--- src/GeoNRage.App/Pages/Statistics.razor.cs
+++ src/GeoNRage.App/Pages/Statistics.razor.cs
@@ -15,11 +15,63 @@ namespace GeoNRage.App.Pages
         [Inject]
         public NavigationManager NavigationManager { get; set; } = null!;
 
-        public IEnumerable<PlayerFullDto> Players { get; set; } = Enumerable.Empty<PlayerFullDto>();
+        internal IEnumerable<PlayerStatistic> Players { get; set; } = Enumerable.Empty<PlayerStatistic>();
 
         protected override async Task OnInitializedAsync()
         {
-            Players = await PlayersApi.GetAllFullAsync();
+            Players = (await PlayersApi.GetAllFullAsync()).Select(CreateStatistic).ToList();
+            Sort(true, nameof(PlayerStatistic.PlayerName));
         }
+
+        private void Sort(bool ascending, string column)
+        {
+            switch (column)
+            {
+                case nameof(PlayerStatistic.PlayerName):
+                    Players = ascending ? Players.OrderBy(p => p.PlayerName) : Players.OrderByDescending(p => p.PlayerName);
+                    break;
+
+                case nameof(PlayerStatistic.NumberOf5000):
+                    Players = ascending ? Players.OrderBy(p => p.NumberOf5000) : Players.OrderByDescending(p => p.NumberOf5000);
+                    break;
+
+                case nameof(PlayerStatistic.NumberOf4999):
+                    Players = ascending ? Players.OrderBy(p => p.NumberOf4999) : Players.OrderByDescending(p => p.NumberOf4999);
+                    break;
+
+                case nameof(PlayerStatistic.ChallengesCompleted):
+                    Players = ascending ? Players.OrderBy(p => p.ChallengesCompleted) : Players.OrderByDescending(p => p.ChallengesCompleted);
+                    break;
+
+                case nameof(PlayerStatistic.BestGame):
+                    Players = ascending ? Players.OrderBy(p => p.BestGame) : Players.OrderByDescending(p => p.BestGame);
+                    break;
+            }
+
+            StateHasChanged();
+        }
+
+        private PlayerStatistic CreateStatistic(PlayerFullDto player)
+        {
+            List<PlayerScoreWithChallengeDto> results = player
+                  .PlayerScores
+                  .GroupBy(p => p.Challenge.GameId)
+                  .OrderByDescending(g => g.OrderBy(c => c.Challenge.Id).Take(3).Select(p => p.Sum).Sum())
+                  .First()
+                  .OrderBy(c => c.Challenge.Id)
+                  .Take(3)
+                  .ToList();
+
+            return new(
+                player.Name,
+                player.Id,
+                player.PlayerScores.SelectMany(p => p.Rounds).Count(s => s == 5000),
+                player.PlayerScores.SelectMany(p => p.Rounds).Count(s => s == 4999),
+                player.PlayerScores.Count(p => p.Rounds.All(s => s is not null or 0)),
+                results.Sum(p => p.Sum) ?? 0,
+                results[0].Challenge.GameId);
+        }
+
+        internal record PlayerStatistic(string PlayerName, string PlayerId, int NumberOf5000, int NumberOf4999, int ChallengesCompleted, int BestGame, int BestGameId);
     }
 }
