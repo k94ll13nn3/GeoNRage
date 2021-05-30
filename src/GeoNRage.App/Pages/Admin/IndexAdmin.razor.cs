993e7f49d0c2af5fa0424534805bdfbef17e30d8
Kévin Gallienne <k94ll13nn3@outlook.fr>
2021-05-30 11:22:41 +0200

    Rework index pages

--- src/GeoNRage.App/Pages/Admin/IndexAdmin.razor.cs
+++ src/GeoNRage.App/Pages/Admin/IndexAdmin.razor.cs
@@ -1,15 +1,15 @@
 ﻿using Microsoft.AspNetCore.Components;
 
-namespace GeoNRage.App.Pages
+namespace GeoNRage.App.Pages.Admin
 {
-    public partial class IndexPage
+    public partial class IndexAdmin
     {
         [Inject]
         public NavigationManager NavigationManager { get; set; } = null!;
 
         protected override void OnInitialized()
         {
-            NavigationManager.NavigateTo("/games");
+            NavigationManager.NavigateTo("/admin/maps");
         }
     }
 }
