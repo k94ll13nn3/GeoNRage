d69abfc6efce7e75c4f5a76162bbaf09a8bfd0a1
Kévin Gallienne <k94ll13nn3@outlook.fr>
2021-06-02 13:25:12 +0200

    Remove leftover from .NET 6 migration

--- src/GeoNRage.App/Pages/GamePage.razor.cs
+++ src/GeoNRage.App/Pages/GamePage.razor.cs
@@ -57,7 +57,6 @@ namespace GeoNRage.App.Pages
             _hubConnection.Reconnecting -= OnHubConnectionReconnecting;
             _hubConnection.Reconnected -= OnHubConnectionReconnected;
             await _hubConnection.DisposeAsync();
-            GC.SuppressFinalize(this);
         }
 
         public async Task ReloadPageAsync()
