006f8a43d9c22253201d5bbdcb9df86f817c2327
Kévin Gallienne <k94ll13nn3@outlook.fr>
2021-06-04 08:16:54 +0200

    Add first version of Table component

---
+++ src/GeoNRage.App/Components/Shared/Table.razor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace GeoNRage.App.Components.Shared
+{
+    public partial class Table<T>
+    {
+        [Parameter]
+        public IEnumerable<T> Items { get; set; } = null!;
+
+        [Parameter]
+        public RenderFragment<T> RowContent { get; set; } = null!;
+
+        [Parameter]
+        public RenderFragment? FooterContent { get; set; }
+
+        [Parameter]
+        public IEnumerable<string> Headers { get; set; } = null!;
+    }
+}
