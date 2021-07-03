741e4ff72fe2b29d4ca83da1944e001f45c18010
Kévin Gallienne <k94ll13nn3@outlook.fr>
2021-07-03 14:54:10 +0200

    Add Expander component

---
+++ src/GeoNRage.App/Components/Shared/Expander.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GeoNRage.App.Components.Shared
+{
+    public partial class Expander
+    {
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        [Parameter]
+        public bool IsExpanded { get; set; }
+
+        [Parameter]
+        public string Title { get; set; } = null!;
+    }
+}
