666951c8093907348620b5220d32706b70235fd8
Kévin Gallienne <k94ll13nn3@outlook.fr>
2021-07-27 19:37:58 +0200

    Update Google models

--- src/GeoNRage.Server/Models/GoogleGeocode.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace GeoNRage.Server.GeoGuessr
-{
-    public class GoogleGeocode
-    {
-        public IList<GoogleGeocodeResult> Results { get; set; } = new List<GoogleGeocodeResult>();
-    }
-}
---
+++ src/GeoNRage.Server/Models/GoogleTypes.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace GeoNRage.Server.Models
+{
+    public record GoogleGeocodeComponent([property: JsonPropertyName("long_name")] string Name, IList<string> Types);
+
+    public record GoogleGeocodeResult(
+        [property: JsonPropertyName("formatted_address")] string FormattedAddress,
+        [property: JsonPropertyName("address_components")] IList<GoogleGeocodeComponent> AddressComponents);
+
+    public record GoogleGeocode(IList<GoogleGeocodeResult> Results);
+}
