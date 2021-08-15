f92b655c4a9bd9a9326144a53546b1274643c709
Kévin Gallienne <k94ll13nn3@outlook.fr>
2021-08-15 13:56:12 +0200

    Restrict SignalR messages to connected users in the game

--- src/GeoNRage.Server/Hubs/AppHub.cs
+++ src/GeoNRage.Server/Hubs/AppHub.cs
@@ -1,4 +1,6 @@
-﻿using GeoNRage.Server.Services;
+﻿using GeoNRage.Server.Entities;
+using GeoNRage.Server.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SignalR;
 
 namespace GeoNRage.Server.Hubs;
@@ -7,16 +9,27 @@ namespace GeoNRage.Server.Hubs;
 public partial class AppHub : Hub
 {
     private readonly GameService _gameService;
+    private readonly UserManager<User> _userManager;
 
     [HubMethodName("JoinGroup")]
-    public Task JoinGroupAsync(int id)
+    public async Task JoinGroupAsync(int gameId)
     {
-        return Groups.AddToGroupAsync(Context.ConnectionId, $"group-${id}");
+        if (!await UserInGame(gameId))
+        {
+            return;
+        }
+
+        await Groups.AddToGroupAsync(Context.ConnectionId, $"group-${gameId}");
     }
 
     [HubMethodName("UpdateValue")]
     public async Task UpdateValueAsync(int gameId, int challengeId, string playerId, int round, int score)
     {
+        if (!await UserInGame(gameId))
+        {
+            return;
+        }
+
         GameDetailDto? game = await _gameService.GetAsync(gameId);
 
         if (game is not null)
@@ -25,5 +38,20 @@ public partial class AppHub : Hub
 
             await Clients.OthersInGroup($"group-${gameId}").SendAsync("ReceiveValue", challengeId, playerId, round, score);
         }
+    }
+
+    private async Task<bool> UserInGame(int gameId)
+    {
+        if (Context.User?.IsInRole(Roles.Member) == true)
+        {
+            GameDetailDto? game = await _gameService.GetAsync(gameId);
+            User user = await _userManager.GetUserAsync(Context.User);
+            if (game?.Players.Any(p => p.Id == user.PlayerId) == true)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
