ef38129b1538bd4e5ca96711619f1c17f01c1e32
Kévin Gallienne <k94ll13nn3@outlook.fr>
2021-10-22 18:09:42 +0200

    Restore default presence since it does not seem to be the cause of the error
    There is a websocket closed error in the logs that should be the source of the reconnection of the bot and the reset of the presence.

--- src/GeoNRage.Server/ServiceCollectionExtensions.cs
+++ src/GeoNRage.Server/ServiceCollectionExtensions.cs
@@ -1,8 +1,12 @@
 using GeoNRage.Server.Bot;
 using GeoNRage.Server.Tasks;
 using Remora.Commands.Extensions;
+using Remora.Discord.API.Abstractions.Objects;
+using Remora.Discord.API.Gateway.Commands;
 using Remora.Discord.Commands.Extensions;
 using Remora.Discord.Gateway.Extensions;
+using Remora.Discord.Gateway;
+using Remora.Discord.API.Objects;
 
 namespace GeoNRage.Server;
 
@@ -21,6 +25,12 @@ public static class ServiceCollectionExtensions
         services.AddDiscordGateway(_ => configuration[$"{nameof(ApplicationOptions)}:{nameof(ApplicationOptions.DiscordBotToken)}"]);
         services.AddDiscordCommands(true);
         services.AddCommandGroup<BotCommands>();
+
+        services.Configure<DiscordGatewayClientOptions>(opt => opt.Presence = new UpdatePresence(
+            ClientStatus.Online,
+            false,
+            null,
+            new[] { new Activity("GeoGuessr", ActivityType.Game) }));
 
         return services;
     }
